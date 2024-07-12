56dde9b1579508c493602abd6c2befec8bff6c9a
unknown <sravanthi.eskeapalli@bluepal.com>
2024-07-12 19:00:21 +0530

    new

---
+++ Python/Data-types/set/set.py
@@ -0,0 +1,15 @@
+name = {"manoj", "ravi", "raja"}
+print(name)
+print(type(name))
+name.add("sai")               # Adding
+print(name)
+name.update(["ravi", "seshu"])   # To add the multiple elements
+print(name)
+delete = name.remove("manoj")
+print(name)
+makrs = {"12", "34", "56"}
+print(makrs.pop())
+print(makrs)
+del name
+print(name)
+  
